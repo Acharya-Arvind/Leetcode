148aeac2cbcb3da90710ed2eea706d84ba905a75
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2024-04-02 16:27:36 +0000

    Sync LeetCode submission Runtime - 43 ms (0.65%), Memory - 17.4 MB (100.00%)

---
+++ 0035-search-insert-position/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        c=0
+        for i in nums:
+            if(i>=target):
+                # c+=1
+                break
+            c+=1
+        return (c)           
+            
+        
