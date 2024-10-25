c67dc1e26c138f673ac7f464f32c0bbeb8d2e59f
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2024-10-25 15:24:59 +0000

    Sync LeetCode submission Runtime - 1735 ms (25.93%), Memory - 17.2 MB (100.00%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -2,8 +2,6 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for j in range(0,len(nums)):
             for i in range(j+1,len(nums)):
-                if(i==len(nums)):
-                    continue
                 if(nums[j]+nums[i]==target):
                     list1=[]
                     list1.append(j)
