79c1ed32bb70e27e8e149ac5b3e24f7f0dc18c97
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2024-11-05 11:04:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 27.7 MB (100.00%)

---
+++ 3226-minimum-number-game/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        vector<int> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i+=2){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i-1]);
+        }
+        return ans;
+    }
+};
