92e850b5b7544534c873447c47908ec1e8e0ca13
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2024-12-14 14:34:19 +0000

    Sync LeetCode submission Runtime - 2 ms (14.29%), Memory - 9.9 MB (10.61%)

---
+++ 0118-pascals-triangle/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans;
+        ans.push_back({1});
+        for(int i=1;i<n;i++){
+            vector<int> ex=ans[i-1];
+            vector<int> res;
+            res.push_back(1);
+            for(int j=1;j<i;j++){
+                res.push_back(ex[j]+ex[j-1]);
+            }
+            res.push_back(1);
+            ans.push_back(res);
+            res.erase(res.begin(),res.end());
+            
+        }
+        return ans;
+    }
+};
