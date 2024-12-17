eb16f1b5a0ab124e87603ff8a466ffaf5c5d016f
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2024-12-17 13:12:09 +0000

    Sync LeetCode submission Runtime - 1606 ms (5.24%), Memory - 389.4 MB (5.51%)

---
+++ 0155-min-stack/solution.cpp
@@ -0,0 +1,45 @@
+class MinStack {
+public:
+    stack<int> s1,s2;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s1.push(val);
+    }
+    
+    void pop() {
+        s1.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        int min=INT_MAX;
+        while(s1.size()>0){
+            s2.push(s1.top());
+            int num=s1.top();
+            s1.pop();
+            if(min>num){
+                min=num;
+            }
+        }
+        while(s2.size()>0){
+            s1.push(s2.top());
+            s2.pop();
+        }
+        return min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
