22b47a7613220f4b69acb8da6dfd920716da96fc
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2025-01-28 06:47:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (86.64%)

---
+++ 0162-find-peak-element/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] m) {
+        int peak=m[0];
+        int ind=0;
+        for(int i=1;i<m.length;i++){
+            if(peak<m[i]){
+                peak=m[i];
+                ind=i;
+            }
+        }
+        return ind;
+    }
+}
