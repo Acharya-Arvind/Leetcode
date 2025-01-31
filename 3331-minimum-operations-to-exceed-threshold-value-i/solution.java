f6902ea35f2e28b95485457858dab78bce952044
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2025-01-31 04:58:30 +0000

    Sync LeetCode submission Runtime - 2 ms (18.46%), Memory - 43.3 MB (14.41%)

---
+++ 3331-minimum-operations-to-exceed-threshold-value-i/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<k) ans++;
+            else return ans;
+        }
+        return 0;
+    }
+}
