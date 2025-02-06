3ef47565c5b97ceb63794b11421a6f7da93be3f1
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2025-02-06 06:44:48 +0000

    Sync LeetCode submission Runtime - 26 ms (28.64%), Memory - 172 MB (44.80%)

--- 2573-remove-nodes-from-linked-list/solution.cpp
+++ 2573-remove-nodes-from-linked-list/solution.cpp
@@ -11,46 +11,33 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        vector<int> num;
+        // vector<int> num;
         ListNode* temp=head;
         ListNode* dummy=new ListNode(0);
         ListNode* cur=dummy;
-        while(temp!=NULL){
-            num.push_back(temp->val);
-            temp=temp->next;
+        ListNode* ptr=head;
+        ListNode* prev=NULL;
+        ListNode* next=NULL;
+        while(ptr){
+            next=ptr->next;
+            ptr->next=prev;
+            prev=ptr;
+            ptr=next;
         }
-        reverse(num.begin(),num.end());
-        vector<int> ans;
-        ans.push_back(num[0]);
-        int maxi=num[0];
-        for(int i=1;i<num.size();i++){
-            if(maxi<=num[i]){
-                ans.push_back(num[i]);
-                maxi=num[i];
+        int maxi=0;
+        ptr=prev;
+        while(ptr){
+            if(maxi<=ptr->val){
+                ListNode* newnode=new ListNode(ptr->val);
+                newnode->next=cur->next;
+                cur->next=newnode;
+                maxi=ptr->val;
             }
+            ptr=ptr->next;
         }
-        for(int i=0;i<ans.size();i++){
-            ListNode* newnode=new ListNode(ans[i]);
-            newnode->next=cur->next;
-            cur->next=newnode;
-        }
-
         
         return dummy->next;
     }
 };
 
-// while(temp!=0){
-        //     ListNode* temp2=temp;
-        //     while(temp2!=NULL){
-        //         if(temp->val<temp2->val){
-        //             break;
-        //         }
-        //         temp2=temp2->next;
-        //     }
-        //     if(temp2==NULL){
-        //         cur->next=temp;
-        //         cur=cur->next;
-        //     }
-        //     temp=temp->next;
-        // }
+
