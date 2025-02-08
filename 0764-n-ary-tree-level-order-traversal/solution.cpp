d2123015c2abd63f1d706666d58f80153a5f72e8
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2025-02-08 04:53:48 +0000

    Sync LeetCode submission Runtime - 18 ms (14.58%), Memory - 16.2 MB (48.89%)

---
+++ 0764-n-ary-tree-level-order-traversal/solution.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        queue<Node*> q;
+        vector<vector<int>> ans;
+        if(!root) return ans;
+        q.push(root);
+        while(q.size()!=0){
+            vector<int> res;
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                 Node* temp=q.front();
+                 q.pop();
+                 res.push_back(temp->val);
+                 for(Node* x:temp->children){
+                    q.push(x);
+                 }
+            }
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
