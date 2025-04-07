4920bb26dc789ecf6b1c3af6a87aafcd51cbbe21
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2025-04-07 05:48:58 +0000

    Sync LeetCode submission Runtime - 4 ms (35.85%), Memory - 15.8 MB (35.59%)

--- 0022-generate-parentheses/solution.cpp
+++ 0022-generate-parentheses/solution.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-
     void backtrack(int open,int close,vector<string>& ans,string s){
         if(open==0 && close==0){
-            
             ans.push_back(s);
-            
             return;
         }
-        if(open>0){
-            backtrack(open-1,close,ans,s+"(");
-        }
-        if(open<close){
-            backtrack(open,close-1,ans,s+")");
-        }
-
+        if(open>0) backtrack(open-1,close,ans,s+"(");
+        if(open<close) backtrack(open,close-1,ans,s+")");
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        if(n==0) return ans;
         backtrack(n,n,ans,"");
         return ans;
     }
