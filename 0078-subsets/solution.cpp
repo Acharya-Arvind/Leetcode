ed2d79dc30ff5daf4cc34080df67febbcee737ea
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2025-04-25 04:26:21 +0000

    Sync LeetCode submission Runtime - 1 ms (46.38%), Memory - 10.3 MB (35.90%)

--- 0078-subsets/solution.cpp
+++ 0078-subsets/solution.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void backtrack(vector<vector<int>>& ans,vector<int>& nums,int ind,vector<int> res){
-        if(ind==nums.size()){
+    void backtrack(vector<int>& nums,vector<vector<int>>& ans,int ind,vector<int> res){
+        if(ind>=nums.size()){
             return;
         }
         for(int i=ind;i<nums.size();i++){
             res.push_back(nums[i]);
             ans.push_back(res);
-            backtrack(ans,nums,i+1,res);
+            backtrack(nums,ans,i+1,res);
             res.pop_back();
         }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         ans.push_back({});
-        backtrack(ans,nums,0,{});
+        backtrack(nums,ans,0,{});
         return ans;
     }
 };
