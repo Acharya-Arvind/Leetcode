1890cb6e5b745461c4f5e7b373f0c1850889923f
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2025-07-08 06:46:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.7 MB (10.50%)

--- 0074-search-a-2d-matrix/solution.java
+++ 0074-search-a-2d-matrix/solution.java
@@ -1,14 +1,11 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
-        }
-        return false;
+     static boolean solve(int[][] mat,int i,int j,int key,int m,int n){
+        if(i==m || j<0) return false;
+        if(mat[i][j]==key) return true;
+        return mat[i][j]<key?solve(mat,i+1,j,key,m,n):solve(mat,i,j-1,key,m,n);
         
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        return solve(matrix,0,matrix[0].length-1,target,matrix.length,matrix[0].length);
     }
 }
