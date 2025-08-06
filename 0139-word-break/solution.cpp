4a73838dbab1854bfceb4376c14e48a839763f5a
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2025-08-06 07:01:06 +0000

    Sync LeetCode submission Runtime - 19 ms (19.81%), Memory - 18.8 MB (16.31%)

--- 0139-word-break/solution.cpp
+++ 0139-word-break/solution.cpp
@@ -1,57 +1,25 @@
-class Tries{
-public:
-  	vector<Tries*> child;
-    bool end=false;
-    Tries(){
-      child.resize(26,NULL);
-    }
-    void insert(Tries* root,string& word){
-      Tries* cur=root;
-      for(char x:word){
-        int ind=x-'a';
-        if(!cur->child[ind]){
-          cur->child[ind]=new Tries();
-        }
-        cur=cur->child[ind];
-      }
-      cur->end=true;
-    }
-    bool search(Tries* root,string& word){
-      Tries* cur=root;
-      for(char x:word){
-        int ind=x-'a';
-        if(!cur->child[ind]){
-          return false;
-        }
-        cur=cur->child[ind];
-      }
-      return cur->end;
-    }
-};
 class Solution {
 public:
-    unordered_map<string,bool> dp;
-    bool solve(Tries* root,string s){
-      int n=s.length();
-      if(n==0) return true;
-      if(dp.find(s)!=dp.end()) return dp[s];
-      for(int i=0;i<n;i++){
-        string prefix=s.substr(0,i+1);
-        string suffix=s.substr(i+1);
-          if(root->search(root,prefix)){
-            if(solve(root,suffix)){
-            return dp[s]=true;
+bool solve(const unordered_set<string>& dict, unordered_map<string, bool>& memo, const string& s) {
+        if (s.length() == 0) return true;
+        if (memo.find(s) != memo.end()) return memo[s];
+        
+        for (int i = 0; i < (int)s.length(); i++) {
+            string t = s.substr(0, i + 1);
+            if (dict.find(t) != dict.end() && solve(dict, memo, s.substr(i + 1))) {
+                return memo[s] = true;
             }
-          }
         }
-      return dp[s]=false;
-
+        
+        return memo[s] = false;
     }
+
     bool wordBreak(string s, vector<string>& wordDict) {
-      Tries* root=new Tries();
-        for(auto x:wordDict){
-          root->insert(root,x);
+        unordered_set<string> dict;
+        for (const string& word : wordDict) {
+            dict.insert(word);
         }
-        return solve(root,s);
+        unordered_map<string, bool> memo;
+        return solve(dict, memo, s);
     }
 };
