f6124faa9644dc7296b307048377643cfa65b138
Arvind Acharya R <160124621+Acharya-Arvind@users.noreply.github.com>
2025-08-20 03:55:50 +0000

    Sync LeetCode submission Runtime - 3 ms (84.37%), Memory - 27.5 MB (86.45%)

---
+++ 1402-count-square-submatrices-with-all-ones/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int ans=0;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if( i && j && matrix[i][j]){
+                    matrix[i][j]=min(matrix[i][j-1],min(matrix[i-1][j],matrix[i-1][j-1]))+1;
+                }
+                ans+=matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
